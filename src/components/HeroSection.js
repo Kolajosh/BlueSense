76b5823093d3dddd2acf6af34e513aa2a087c0c3
Kolajosh <ayoolak@ymail.com>
2021-11-13 23:35:55 +0100

    updated fonts

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,13 +10,15 @@ function HeroSection() {
         <div className='hero-container'>
             <img src={home} alt='homepic'></img>
             <h1>Thinking about transforming<br></br>your company? We know how</h1>
-            <p>BlueSense is a business intelligence management consulting firm. We<br></br> 
-                are the trusted advisors to some of the country&rsquo;s leading startups,<br></br>  
-                technology companies and a handful of international institutions. We<br></br> 
-                work with leading organisations like yours across private, public and<br></br> 
-                nonprofit sectors to produce bankable results. Our expertise,<br></br> 
-                experience, scope, and knowledge of organisational challenges allow<br></br> 
-                us to address problems that no one else can.</p><br></br> 
+            <div className='hero-text-container'>
+            <p>BlueSense is a business intelligence management consulting firm. We 
+                are the trusted advisors to some of the country&rsquo;s leading startups,  
+                technology companies and a handful of international institutions. We 
+                work with leading organisations like yours across private, public and 
+                nonprofit sectors to produce bankable results. Our expertise, 
+                experience, scope, and knowledge of organisational challenges allow 
+                us to address problems that no one else can.</p> 
+                </div>
                 <div className='hero-btns'>
                     <Button className='btns' buttonStyle='btn--primary' butttonSize='btn--large'>
                         <Link to ='/contact' className='btn-link'> 
