4e1f8ff8d666d6a08c432648a9c559e3b910849a
KolaJosh <ayoolak@ymail.com>
2022-03-03 16:19:52 +0100

    new blog update

--- src/components/blogPages/InternationalOilCompaies.js
+++ src/components/blogPages/InternationalOilCompaies.js
@@ -47,24 +47,30 @@ function InternationalOilCompanies() {
       </div>
       <div className="blog1-text">
         <p>
+          Ever since oil was discovered in Nigeria, Nigeria saw international
+          oil companies coming into the country to set up branches for crude oil
+          exploration which served as a source of revenue for the government and
+          also boosted their economy. However, over the last few years,
+          international oil companies are gradually divesting from Nigeria.{" "}
+          <br />
+          <br />
           Chevron intends to sell some of its Nigerian oil fields as part of a
           global effort to restructure its portfolio and focus on boosting its
           shale production in the United States. The business, which is
-          Nigeria’s third-largest oil producer, is seeking buyers for some of
+          Nigeria's third-largest oil producer, is seeking buyers for some of
           its onshore and shallow offshore assets. Chevron is apparently in
           direct contact with possible purchasers and does not intend to conduct
           a bidding procedure for the assets. Chevron auctioned around 40% of
           its ownership in selected infrastructure in 2013 to improve capital
           efficiency and provide prospective purchasers with the opportunity to
-          build their own assets. <br />
+          build their assets. <br />
           <br />
           For more than a decade, Shell has progressively sold its onshore
-          assets in order to avoid issues, like pollution caused by damaged
-          pipelines and the ensuing legal fights with local communities,
-          etcetera. The issues became urgent in 2020 when Shell announced its
-          intention to transition into a clean energy Behemoth and progressively
-          phase down its oil and gas operations in order to reach net-zero
-          carbon emissions by 2050.. <br />
+          assets to avoid issues, like pollution caused by damaged pipelines and
+          the ensuing legal fights with local communities, etcetera. The issues
+          became urgent in 2020 when Shell announced its intention to transition
+          into a clean energy Behemoth and progressively phase down its oil and
+          gas operations to reach net-zero carbon emissions by 2050. <br />
           <br />
           Notably, ExxonMobil sold its stake in the Joint Venture shallow-water
           project with NNPC to Seplat Energy but reaffirmed its commitment to
@@ -72,37 +78,42 @@ function InternationalOilCompanies() {
           holdings, while Brazilian oil company Petrobras informed Nigeria that
           it will sell its 8% ownership in the Agbami block and 20% in the
           offshore Akpo project for ₦795 billion. Total Energies divested its
-          20% ownership in the Niger Delta’s Usan field. ConocoPhillips also
+          20% ownership in the Niger Delta's Usan field. ConocoPhillips also
           sold its onshore assets and exited Nigeria following the completion of
           the $1.5 billion sales of its oil assets to Oando. <br />
           <br />
           There has been considerable pressure on oil companies to transition to
-          green energy. In Europe, investors, stakeholders, and activists,
+          green energy. In Europe, investors, stakeholders, and activists
           successfully compelled several MNCs to pull back crude drilling
-          through litigation, lobbying, and protests. <br />
+          through litigation, lobbying, and protests.
           <br />
-          The ‘green shift’ and corporate repositioning have compelled oil
-          corporations to rethink their investment objectives in a number of
+          <br />
+          The 'green shift' and corporate repositioning have compelled oil
+          corporations to rethink their investment objectives in several
           nations, including Nigeria. Nigeria has been a long-time opponent of
           oil reforms, so the IOCs saw the country as anti-futuristic, which led
-          to more talk about a gradual withdrawal from the country’s crude
+          to more talk about a gradual withdrawal from the country's crude
           exploration business.
-          <br />
           <br />
           This has led to the acquisition of IOCs assets by indigenous companies
           in Nigeria such as Seplat Energy, TNOG Oil and Gas Ltd, MRS Holdings,
           and Conoil Plc. This recent initiative would force local Nigerian oil
-          companies to significantly increase their own production capacity and
-          financial muscle in order to become more engaged in the sector.
+          companies to significantly increase their production capacity and
+          financial muscle to become more engaged in the sector. <br />
           <br />
-          <br />
-          However, there has been scepticism about indigenous companies’ ability
+          However, there has been scepticism about indigenous companies' ability
           to take over onshore assets from IOCs in light of ongoing divestment
           issues. Aside from existing issues of vandalism and theft, legal
           battles, ageing infrastructure and ongoing battles over general clean
           up and compensation for oil and gas producing communities are among
           the top challenges that could make or break the majority of indigenous
           companies.
+          <br />
+          <br />
+          While the purchase of IOCs infrastructure by local oil companies is
+          the right step, which could increase production capacity, it begs the
+          question of why local oil companies are focused on infrastructure,
+          rather than developing green energy in Nigeria.
         </p>
       </div>
       <div className="flex2">
