f3f5d4b7890021a44f0216983cf0b6b5398ec7f6
KolaJosh <ayoolak@ymail.com>
2022-03-17 15:49:15 +0100

    new advisers

--- src/components/AdvisersIcon.js
+++ src/components/AdvisersIcon.js
@@ -1,6 +1,9 @@
 import React from "react";
 import olaleye from "../images/Mask Group-1.png";
 import garza from "../images/Mask Group-2.png";
+import new1 from "../images/new1.png";
+import new2 from "../images/new2.png";
+
 import "./AdvisersIcon.css";
 
 function PartnerIcons1() {
@@ -52,11 +55,59 @@ function PartnerIcons1() {
           </div>
           <li>
             <p>Andrew Garza</p>
-
             <p className="blue">
               Ex. Bain(5yrs)
               <br />
               Stanford MBA
+            </p>
+          </li>
+        </div>
+
+        <div className="adviser1">
+          <img src={new2} alt=""></img>
+          <div className="advisers-overlay">
+            <p>
+              <h4>Co-founder</h4>
+              <br />
+              Nadayar is co-founder and CEO at Eden Life, a tech-enabled
+              services company that automates home chores. Driven by a passion
+              for technology, Nadayar had previously co-founded Fora + Andela.
+              He has a proven track record of building enterprise software.
+              <br></br>
+              <br></br>
+              <i class="fab fa-linkedin"></i>
+            </p>
+          </div>
+          <li>
+            <p>Nadayar Enegesi</p>
+            <p className="blue">
+              Technology
+              <br />
+              Advisory
+            </p>
+          </li>
+        </div>
+
+        <div className="adviser1">
+          <img src={new1} alt=""></img>
+          <div className="advisers-overlay">
+            <p>
+              <br />
+              Lisa is Project Lead, Humanitarian + Resilience Investing at the
+              World Economic Forum (WEF). Previously she was Programme Manager,
+              Private Sector Development and Innovation and a Blue Book Trainee,
+              Cabinet of Jean-Claude Juncker at the European Commission.
+              <br></br>
+              <br></br>
+              <i class="fab fa-linkedin"></i>
+            </p>
+          </div>
+          <li>
+            <p>Lisa Satolli</p>
+            <p className="blue">
+              Economics
+              <br />
+              Advisory
             </p>
           </li>
         </div>
